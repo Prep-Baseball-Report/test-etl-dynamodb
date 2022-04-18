7f744965e4d63a863c877c4ae44d74cb2ce2f3a5
eitanleff1 <eitanleff@prepbaseballreport.com>
2022-04-18 00:34:34 -0400

    delete lambdas

--- infra/lambdas/get_time/lambda_function.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-def lambda_handler(event, context):
-    if not event.get('backfill_timestamp'):
-            timestamp = datetime.utcnow() - timedelta(hours = 2)
-            timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-    else:
-        timestamp = event['backfill_timestamp']
-    return timestamp
--- infra/lambdas/wait_timer_backoff/lambda_function.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print(event)
-    event['waitSeconds']=event['waitSeconds']*2
-    return event
